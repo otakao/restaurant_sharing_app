ea31f5081b39c21a7324021480c3d029e713089f
otakao <kazuhawk@gmail.com>
2020-09-12 15:14:31 +0900

    users-update-file

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,12 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @posts = Post.where(user_id: params[:id]).includes(:user).order(created_at: :desc)    
+    @address = @user.address
+
+    @images = []
+    @posts.each do |post|
+      @images << post.images
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   get 'addresses/create'
   get 'addresses/new'
   get 'addresses/edi'
