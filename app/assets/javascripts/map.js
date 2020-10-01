62b4ea8df630f45b6641d55ed6601d6438a8d6e1
otakao <kazuhawk@gmail.com>
2020-10-01 13:23:10 +0900

    Update map.js

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,57 +1,57 @@
 
-  let map
-  let geocoder
+  // let map
+  // let geocoder
 
-  function initMap(){
-    geocoder = new google.maps.Geocoder()
+  // function initMap(){
+  //   geocoder = new google.maps.Geocoder()
 
-    mapInstance = new google.maps.Map(document.getElementById('map'),{
-      center: {lat: 35.681, lng:139.767},
-      zoom: 14
-    });
+  //   mapInstance = new google.maps.Map(document.getElementById('map'),{
+  //     center: {lat: 35.681, lng:139.767},
+  //     zoom: 14
+  //   });
 
-    <% @maps.each do |map| %>
-    pos = new google.maps.Latlng(
-      <%=map.latitude%>
-      <%=map.longitude%>    
-    );
+  //   <% @maps.each do |map| %>
+  //   pos = new google.maps.Latlng(
+  //     <%=map.latitude%>
+  //     <%=map.longitude%>    
+  //   );
 
-    default_marker = new google.maps.Marker({
-      map: mapInstance,
-      position: pos,
-      icon: {
-        url: ' https://maps.google.com/mapfiles/ms/icons/green-dot.png',
-        scaledSize: new google.maps.Size(40, 40)
-      }
-    });
-    <%end%>
-  }
+  //   default_marker = new google.maps.Marker({
+  //     map: mapInstance,
+  //     position: pos,
+  //     icon: {
+  //       url: ' https://maps.google.com/mapfiles/ms/icons/green-dot.png',
+  //       scaledSize: new google.maps.Size(40, 40)
+  //     }
+  //   });
+  //   <%end%>
+  // }
 
-  function codeAddress(){
-    let inputAddress = document.getElementById('address').value;
-    geocoder.geocode( { 'address' : inputAddress}, function(results, status) {
-      if (status == 'OK') {
-        map.setCenter(results[0].geometry.location);
+  // function codeAddress(){
+  //   let inputAddress = document.getElementById('address').value;
+  //   geocoder.geocode( { 'address' : inputAddress}, function(results, status) {
+  //     if (status == 'OK') {
+  //       map.setCenter(results[0].geometry.location);
 
-        if(marker != null){
-          marker.setMap(null); 
-        }
-        marker = null;
+  //       if(marker != null){
+  //         marker.setMap(null); 
+  //       }
+  //       marker = null;
 
-        marker = new google.maps.Marker({
-          map: mapInstance,
-          position: results[0].geometry.location
-        });
+  //       marker = new google.maps.Marker({
+  //         map: mapInstance,
+  //         position: results[0].geometry.location
+  //       });
 
-        let title = document.getElementById('map_title');
-        title.setAttribute("value", inputAddress);
+  //       let title = document.getElementById('map_title');
+  //       title.setAttribute("value", inputAddress);
 
-        let hidden_address = document.getElementById('hidden_address');
-        hidden_address.setAttribute("value", inputAddress);
+  //       let hidden_address = document.getElementById('hidden_address');
+  //       hidden_address.setAttribute("value", inputAddress);
 
-      } else {
-        alert('Geocode was not successful for the following reason: ' + status);
-      }
-    });
-  }
+  //     } else {
+  //       alert('Geocode was not successful for the following reason: ' + status);
+  //     }
+  //   });
+  // }
 
